8f27a7c9597219c0c06efd7412df9da7f9a3e9b9
alijr2018 <alijr_2018@proton.me>
2024-01-31 16:06:53 +0100

    ?

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -13,34 +13,54 @@ class MRUCache(BaseCaching):
     class MRUCache that inherits from,
     BaseCaching and is a caching system.
     """
-
     def __init__(self):
         """
         the MRUCache instance
         """
         super().__init__()
-        self.mru_tracker = OrderedDict()
+        self.head, self.tail = 'head', 'tail'
+        self.next, self.prev = {}, {}
+        self.handle(self.head, self.tail)
 
-    def put(self, key, item):
+    def handle(self, head, tail):
         """
         Add item to the cache
         """
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                self.mru_tracker.move_to_end(key)
-            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                mru_key, _ = self.mru_tracker.popitem(last=False)
-                del self.cache_data[mru_key]
-                print("DISCARD:", mru_key)
+        self.next[head], self.prev[tail] = tail, head
 
-            self.cache_data[key] = item
-            self.mru_tracker[key] = datetime.now()
+    def _remove(self, key):
+        """ MRU algorithm, remove element """
+        self.handle(self.prev[key], self.next[key])
+        del self.prev[key], self.next[key], self.cache_data[key]
+
+    def add_to(self, key, item):
+        """
+        add
+        """
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
+            print("DISCARD: {}".format(self.prev[self.tail]))
+            self._remove(self.prev[self.tail])
+        self.cache_data[key] = item
+        self.handle(self.prev[self.tail], key)
+        self.handle(key, self.tail)
+
+    def put_to(self, key, item):
+        """
+        put to dictionary
+        """
+        if key and item:
+            if key in self.cache_data:
+                self._remove(key)
+            self.add_to(key, item)
 
     def get(self, key):
         """
         Get an item from the cache
         """
-        if key is not None and key in self.cache_data:
-            self.mru_tracker.move_to_end(key)
-            return self.cache_data[key]
-        return None
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        if key in self.cache_data:
+            value = self.cache_data[key]
+            self._remove(key)
+            self.add_to(key, value)
+            return value
