ebc74ca4db35722e117f453076662888ec3e1796
alijr2018 <alijr_2018@proton.me>
2024-02-06 22:21:02 +0100

    chekc

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -2,7 +2,7 @@
 """
 5-app.py
 """
-from flask import Flask, request, render_template, g
+from flask import Flask, render_template, g, request
 from flask_babel import Babel, _
 
 app = Flask(__name__)
@@ -15,29 +15,29 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
-def get_user(user_id):
-    return users.get(user_id)
 
 @app.before_request
 def before_request():
     user_id = int(request.args.get('login_as', 0))
-    g.user = get_user(user_id)
-    print(f"before_request - User ID: {user_id}, User: {g.user}")
+
+    g.user = users.get(user_id, None)
+
 
 app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'fr']
 
+
 @babel.localeselector
 def get_locale():
     if g.user and g.user['locale'] in app.config['BABEL_SUPPORTED_LOCALES']:
-        print(f"get_locale - User Locale: {g.user['locale']}")
         return g.user['locale']
-    print("get_locale - Default Locale")
     return app.config['BABEL_DEFAULT_LOCALE']
+
 
 @app.route('/')
 def home():
-    return render_template('5-index.html')
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
