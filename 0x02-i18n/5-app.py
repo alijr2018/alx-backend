216094583b540d2982fa95a751ddb6337776fca6
alijr2018 <alijr_2018@proton.me>
2024-02-07 00:30:54 +0100

    lol

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -3,7 +3,7 @@
 5-app.py
 """
 from flask import Flask, render_template, g, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -15,51 +15,39 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
-@babel.localeselector
-def get_locale():
-    """
-    Get the user's preferred locale.
-    """
-    url_locale = request.args.get('locale')
-    if url_locale and url_locale in app.config['BABEL_SUPPORTED_LOCALES']:
-        return url_locale
-
-    if g.user and g.user.get('locale'):
-        return g.user['locale']
-
-    header_locale = request.headers.get('Accept-Language')
-    if header_locale:
-        for lang in header_locale.replace(' ', '').split(','):
-            if lang in app.config['BABEL_SUPPORTED_LOCALES']:
-                return lang
-
-    return app.config['BABEL_DEFAULT_LOCALE']
 
 @app.before_request
 def before_request():
     """
-    Function to be executed before handling each request.
+    function and use the app.before_request,
+    decorator to make it be executed before al
     """
     user_id = int(request.args.get('login_as', 0))
-    g.user = get_user(user_id)
 
-def get_user(user_id):
+    g.user = users.get(user_id, None)
+
+
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'fr']
+
+
+@babel.localeselector
+def get_locale():
     """
-    Get user dictionary based on user ID.
+    get language form local
     """
-    return users.get(user_id, None)
+    if g.user and g.user['locale'] in app.config['BABEL_SUPPORTED_LOCALES']:
+        return g.user['locale']
+    return app.config['BABEL_DEFAULT_LOCALE']
+
 
 @app.route('/')
 def index():
     """
-    Render the index template with the appropriate message.
+    render templates
     """
-    if g.user:
-        message = _("You are logged in as %(username)s.") % {"username": g.user['name']}
-    else:
-        message = _("You are not logged in.")
-    
-    return render_template('index.html', message=message)
+    return render_template('5-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
