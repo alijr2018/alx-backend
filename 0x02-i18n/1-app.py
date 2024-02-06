8152cdf10b59f7af4b2de6e12ed861ca2ae7bce3
alijr2018 <alijr_2018@proton.me>
2024-02-06 12:15:09 +0100

    check 1-app.py

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+1-app.py
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    """
+    route
+    """
+    return render_template('0-index.html')
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+babel.init_app(app)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
