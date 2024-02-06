c4386dd230c96e169110dff0ad8b14590731ebe3
alijr2018 <alijr_2018@proton.me>
2024-02-06 22:36:29 +0100

    lol

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -22,7 +22,8 @@ def get_locale():
     if req_local and req_local in app.config['BABEL_SUPPORTED_LOCALES']:
         return req_local
     else:
-        return request.accept_languages.best_match(app.config['BABEL_SUPPORTED_LOCALES'])
+        return request.accept_languages.best_match(app.config
+                                                   ['BABEL_SUPPORTED_LOCALES'])
 
 
 @app.route('/')
