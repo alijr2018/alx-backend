0346f4df0df4c52a3b661273833a3de2c31eb13c
alijr2018 <alijr_2018@proton.me>
2024-02-06 23:43:34 +0100

    know

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -5,6 +5,16 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
+
+class Config(object):
+    """
+    class Config
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
 app = Flask(__name__)
 babel = Babel(app)
 
