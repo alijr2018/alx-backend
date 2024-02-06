b6ddecd8e7861ef2b729b81a8e2223826d0cf2d5
alijr2018 <alijr_2018@proton.me>
2024-02-06 23:01:07 +0100

    ?

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -13,7 +13,7 @@ app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'fr']
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """
     get language form local
     """
@@ -27,7 +27,7 @@ def get_locale():
 
 
 @app.route('/')
-def home():
+def home() -> str:
     """
     render templates
     """
