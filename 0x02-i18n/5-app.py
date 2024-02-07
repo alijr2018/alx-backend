740416bef40d765c1ed6b1c976ebc871c498f1a5
alijr2018 <alijr_2018@proton.me>
2024-02-07 01:20:26 +0100

    check#

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,12 +1,44 @@
 #!/usr/bin/env python3
 """
-5-app.py
+A Basic flask application
 """
-from flask import Flask, render_template, g, request
+from typing import (
+    Dict, Union
+)
+
+from flask import Flask
+from flask import g, request
+from flask import render_template
 from flask_babel import Babel
 
+
+class Config(object):
+    """
+    Application configuration class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# Instantiate the application object
 app = Flask(__name__)
+app.config.from_object(Config)
+
+# Wrap the application with Babel
 babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Gets locale from request object
+    """
+    locale = request.args.get('locale', '').strip()
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -16,38 +48,32 @@ users = {
 }
 
 
+def get_user(id) -> Union[Dict[str, Union[str, None]], None]:
+    """
+    Validate user login details
+    Args:
+        id (str): user id
+    Returns:
+        (Dict): user dictionary if id is valid else None
+    """
+    return users.get(int(id), 0)
+
+
 @app.before_request
 def before_request():
     """
-    function and use the app.before_request,
-    decorator to make it be executed before al
+    Adds valid user to the global session object `g`
     """
-    user_id = int(request.args.get('login_as', 0))
-
-    g.user = users.get(user_id, None)
+    setattr(g, 'user', get_user(request.args.get('login_as', 0)))
 
 
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'fr']
-
-
-@babel.localeselector
-def get_locale():
+@app.route('/', strict_slashes=False)
+def index() -> str:
     """
-    get language form local
-    """
-    if g.user and g.user['locale'] in app.config['BABEL_SUPPORTED_LOCALES']:
-        return g.user['locale']
-    return app.config['BABEL_DEFAULT_LOCALE']
-
-
-@app.route('/')
-def index():
-    """
-    render templates
+    Renders a basic html template
     """
     return render_template('5-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
